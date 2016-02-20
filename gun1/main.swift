e3bdc653533ce589a7990758ac220b56dfbd466c
MacBook <macbook@macbooks-macbook-pro.local>
2016-02-20 10:31:20 +0200

    Initial Commit

---
+++ gun1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gun1
+//
+//  Created by MacBook on 20/02/16.
+//  Copyright © 2016 ilkuygulamagithub. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
